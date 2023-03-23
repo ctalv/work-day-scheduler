c5ac3645b03b31817a4b080d9682c90c809c88ac
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-22 22:35:03 -0500

    added query selector for save buttons; added crude event listener for save button

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 // in the html.
 $(function () {
   var today = dayjs();
+  var timeblockClass = $('.time-block');
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
+var saveButtonEl = $('.btn')
+saveButtonEl.on('click', saveText());
+
+function saveText() {
+  console.log("saved")
+}
+  
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
@@ -19,7 +26,6 @@ $(function () {
     var hour = today.format('h');
     var amPm = today.format('a');
 
-    var timeblockClass = $('.time-block');
     var businessHours = [];
     var hourCompare = "hour-"+hour;
     // TESTING Hardcode
