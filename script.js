a46c2f59a5059e5aec3fe0b75c3672e51ba3282a
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-24 14:28:14 -0500

    added both methods of color code for testing

--- script.js
+++ script.js
@@ -48,6 +48,14 @@ $(function () {
 
   }
 
+
+
+
+  /*
+
+  COLOR CODE - NEW METHOD
+
+*
   var indexPoint = 0;
 
   function colorCode() {
@@ -99,6 +107,61 @@ $(function () {
       }
     }
   }
+
+ // */
+
+
+/*
+
+COLOR CODE - ORIGINAL METHOD
+
+*
+  function colorCode() {
+    var hour = today.format('h');
+    var amPm = today.format('a');
+
+    var businessHours = [];
+    var hourCompare = "hour-" + hour;
+
+    // TESTING HARDCODE
+    // hourCompare = "hour-1";
+    // hour = 4
+    // amPm = 'am'
+
+    for (i = 0; i < 9; i++) {
+
+      var hourId = timeblockClass[i].id;
+      businessHours.push(hourId);
+
+      // compare if indexed hour is the same as current hour
+      if (hourCompare === hourId) {
+        timeblockClass[i].classList.add("present");
+        // indexPoint to build past and future off of
+        var indexPoint = i;
+
+        for (i = 0; i < 9; i++) {
+          // if user is using webpage not during business hours
+          if ((hour === 6 || 7 || 8 || 9 || 10 || 11) && (amPm === 'pm')) { // evening
+            timeblockClass[indexPoint].classList.remove("present");
+            timeblockClass[i].classList.add("past");
+          } else if ((hour === 12 || 1 || 2 || 3 || 4 || 5) && (amPm === 'am')) { // morning
+            timeblockClass[indexPoint].classList.remove("present");
+            timeblockClass[i].classList.add("future");
+          } else {
+            // if user is using webpage during business hours
+            if (i < indexPoint) {
+              timeblockClass[i].classList.add("past");
+            } else if (i > indexPoint) {
+              timeblockClass[i].classList.add("future");
+            }
+          }
+        }
+      }
+
+    }
+
+  }
+ // */
 
   // console.log(indexPoint)
 
