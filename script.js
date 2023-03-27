203c206ca2da504974045a9bd83db96d647b8716
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-27 12:04:16 -0500

    removed all console logs and commented out code

--- script.js
+++ script.js
@@ -29,8 +29,6 @@ $(function () {
     }
   }
 
-
-
   function enterEvent() {
 
     for (i = 0; i < 9; i++) {
@@ -48,208 +46,48 @@ $(function () {
 
   }
 
-
   function colorCode() {
     var hour = today.format('h');
     var amPm = today.format('a');
 
     // TESTING HARDCODE
     // hourCompare = "hour-1";
-    hour = 1
-    amPm = 'pm'
+    // hour = 1
+    // amPm = 'pm'
 
     var businessHours = [];
     var hourCompare = "hour-" + hour;
-
-
-    console.log(hour)
-    console.log(amPm)
 
     if ((amPm === 'pm') && // evening
     ((hour === 6) || (hour ===7) || (hour ===8) || (hour ===9) || (hour ===10) || (hour ===11) )) { 
       for (j = 0; j < timeblockClass.length; j++ ){
       timeblockClass[j].classList.add("past");
-      console.log('1')
       }
     } else if ((amPm === 'am') && // morning
     ((hour === 12) || (hour ===1)||(hour===2)||(hour===3)||(hour===4)||(hour===5)||(hour===6)||(hour===7)||(hour===8))) { 
       for (j = 0; j < timeblockClass.length; j++ ){
         timeblockClass[j].classList.add("future");
-        console.log('2')
-        console.log(hour)
-        console.log(amPm)
         }
     } else {
-      console.log('3')
-      console.log(hour)
-      console.log(amPm)
       for (i = 0; i < timeblockClass.length; i++) {
-
         var hourId = timeblockClass[i].id;
         businessHours.push(hourId);
-        console.log(i)
-        console.log(hourCompare)
-        console.log(hourId)
-        console.log(businessHours)
 
         // compare if indexed hour is the same as current hour
         if (hourCompare === hourId) {
           timeblockClass[i].classList.add("present");
           // indexPoint to build past and future off of
           var indexPoint = i;
-          console.log(indexPoint)
         } else {
           if (i > indexPoint) {
             timeblockClass[i].classList.add("future");
-            console.log('future', i)
           } else {
             timeblockClass[i].classList.add("past");
-            console.log('past',i)
           }
         }
       }
     }
   }
-
-
-  /*
-
-  COLOR CODE - NEW METHOD
-
-*
-  var indexPoint = 0;
-
-  function colorCode() {
-    var hour = today.format('h');
-    var amPm = today.format('a');
-
-    var businessHours = [];
-    var hourCompare = "hour-" + hour;
-
-    // TESTING HARDCODE
-    // hourCompare = "hour-1";
-    // hour = 4
-    // amPm = 'am'
-    console.log(hour)
-    console.log(amPm)
-
-    if (hour === (6 || 7 || 8 || 9 || 10 || 11) && amPm === 'pm') { // evening
-      for (j = 0; j < timeblockClass.length; j++ ){
-      timeblockClass[j].classList.add("past");
-      console.log('why')
-      }
-    } else if ((hour === 12 || 1 || 2 || 3 || 4 || 5) && (amPm === 'am')) { // morning
-      for (j = 0; j < timeblockClass.length; j++ ){
-        timeblockClass[j].classList.add("furture");
-        console.log('why')
-        }
-    } else {
-
-      for (i = 0; i < 9; i++) {
-
-        var hourId = timeblockClass[i].id;
-        businessHours.push(hourId);
-
-        // compare if indexed hour is the same as current hour
-        if (hourCompare === hourId) {
-          timeblockClass[i].classList.add("present");
-          // indexPoint to build past and future off of
-          indexPoint = i;
-          console.log(indexPoint)
-        } else {
-          if (i < indexPoint) {
-            timeblockClass[i].classList.add("past");
-            console.log('past',i)
-          } else if (i > indexPoint) {
-            timeblockClass[i].classList.add("future");
-            console.log('future', i)
-          }
-        }
-      }
-    }
-  }
-
- // */
-
-
-/*
-
-COLOR CODE - ORIGINAL METHOD
-
-*
-  function colorCode() {
-    var hour = today.format('h');
-    var amPm = today.format('a');
-
-    var businessHours = [];
-    var hourCompare = "hour-" + hour;
-
-    // TESTING HARDCODE
-    // hourCompare = "hour-1";
-    // hour = 4
-    // amPm = 'am'
-
-    for (i = 0; i < 9; i++) {
-
-      var hourId = timeblockClass[i].id;
-      businessHours.push(hourId);
-
-      // compare if indexed hour is the same as current hour
-      if (hourCompare === hourId) {
-        timeblockClass[i].classList.add("present");
-        // indexPoint to build past and future off of
-        var indexPoint = i;
-
-        for (i = 0; i < 9; i++) {
-          // if user is using webpage not during business hours
-          if ((hour === 6 || 7 || 8 || 9 || 10 || 11) && (amPm === 'pm')) { // evening
-            timeblockClass[indexPoint].classList.remove("present");
-            timeblockClass[i].classList.add("past");
-          } else if ((hour === 12 || 1 || 2 || 3 || 4 || 5) && (amPm === 'am')) { // morning
-            timeblockClass[indexPoint].classList.remove("present");
-            timeblockClass[i].classList.add("future");
-          } else {
-            // if user is using webpage during business hours
-            if (i < indexPoint) {
-              timeblockClass[i].classList.add("past");
-            } else if (i > indexPoint) {
-              timeblockClass[i].classList.add("future");
-            }
-          }
-        }
-      }
-
-    }
-
-  }
- // */
-
-  // console.log(indexPoint)
-
-  // function colorCode() {
-
-  //   for (j = 0; j < 9; j++) {
-  //     // if user is using webpage not during business hours
-  //     if ((hour === 6 || 7 || 8 || 9 || 10 || 11) && (amPm === 'pm')) { // evening
-  //       timeblockClass[indexPoint].classList.remove("present");
-  //       timeblockClass[j].classList.add("past");
-  //     } else if ((hour === 12 || 1 || 2 || 3 || 4 || 5) && (amPm === 'am')) { // morning
-  //       timeblockClass[indexPoint].classList.remove("present");
-  //       timeblockClass[j].classList.add("future");
-  //     } else {
-  //       // if user is using webpage during business hours
-  //       if (j < indexPoint) {
-  //         timeblockClass[j].classList.add("past");
-  //       } else if (j > indexPoint) {
-  //         timeblockClass[j].classList.add("future");
-  //       }
-  //     }
-  //   }
-  // }
-
-
-
-
 
   // display the current date in the header of the page.
   $('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
