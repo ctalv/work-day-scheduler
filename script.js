02c50c0a750b67083f7f1c09962d2c232f2b0b3f
ctalv <122413805+ctalv@users.noreply.github.com>
2023-03-27 12:18:06 -0500

    added comments and cleaned up code; added screenshot of webpage

--- script.js
+++ script.js
@@ -1,14 +1,11 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
   var today = dayjs();
   var timeblockClass = $('.time-block');
   var saveButtontEl = $('button')
 
-
+  // check if anything already stored in local storage or not
   if (window.localStorage.getItem('savedTextObj') === null) {
-    var savedTextObj = {
+    var savedTextObj = { // create save object variable
       'hour-9': '',
       'hour-10': '',
       'hour-11': '',
@@ -20,32 +17,32 @@ $(function () {
       'hour-5': '',
     }
   } else {
-    var savedTextObj = JSON.parse(window.localStorage.getItem("savedTextObj"))
-    for (i = 0; i < timeblockClass.length; i++) {
+    var savedTextObj = JSON.parse(window.localStorage.getItem("savedTextObj")) // pull what is in local storage
+    for (i = 0; i < timeblockClass.length; i++) { // plave text back into text boxes
       var savedTextSingle = savedTextObj[timeblockClass[i].id];
       var textareaAddBack = timeblockClass[i].children[1];
-      textareaAddBack.innerHTML = savedTextSingle;
+      textareaAddBack.textContent = savedTextSingle;
 
     }
   }
 
-  function enterEvent() {
-
+  // pressing a save button
+  function saveEvent() {
     for (i = 0; i < 9; i++) {
-      // at each button
-      var button = saveButtontEl[i]
-      // create an event listener to save text at a particular time
-      button.addEventListener('click', function () {
+      var button = saveButtontEl[i] // at each button
+
+      button.addEventListener('click', function () { // create an event listener to save text at a particular time
         var clickedSaveButton = this.previousElementSibling.value;
         var textareaHTML = this.previousElementSibling;
         textareaHTML.textContent = clickedSaveButton
         savedTextObj[textareaHTML.parentNode.id] = clickedSaveButton
-        window.localStorage.setItem('savedTextObj', JSON.stringify(savedTextObj));
+        window.localStorage.setItem('savedTextObj', JSON.stringify(savedTextObj)); // store text entered
       })
     }
 
   }
 
+  // colors the corresponding textarea with past, present, or future (grey, red, or green respectively)
   function colorCode() {
     var hour = today.format('h');
     var amPm = today.format('a');
@@ -59,29 +56,26 @@ $(function () {
     var hourCompare = "hour-" + hour;
 
     if ((amPm === 'pm') && // evening
-    ((hour === 6) || (hour ===7) || (hour ===8) || (hour ===9) || (hour ===10) || (hour ===11) )) { 
-      for (j = 0; j < timeblockClass.length; j++ ){
-      timeblockClass[j].classList.add("past");
+      ((hour === 6) || (hour === 7) || (hour === 8) || (hour === 9) || (hour === 10) || (hour === 11))) {
+      for (j = 0; j < timeblockClass.length; j++) {
+        timeblockClass[j].classList.add("past");
       }
     } else if ((amPm === 'am') && // morning
-    ((hour === 12) || (hour ===1)||(hour===2)||(hour===3)||(hour===4)||(hour===5)||(hour===6)||(hour===7)||(hour===8))) { 
-      for (j = 0; j < timeblockClass.length; j++ ){
+      ((hour === 12) || (hour === 1) || (hour === 2) || (hour === 3) || (hour === 4) || (hour === 5) || (hour === 6) || (hour === 7) || (hour === 8))) {
+      for (j = 0; j < timeblockClass.length; j++) {
         timeblockClass[j].classList.add("future");
-        }
+      }
     } else {
       for (i = 0; i < timeblockClass.length; i++) {
         var hourId = timeblockClass[i].id;
         businessHours.push(hourId);
-
-        // compare if indexed hour is the same as current hour
         if (hourCompare === hourId) {
           timeblockClass[i].classList.add("present");
-          // indexPoint to build past and future off of
-          var indexPoint = i;
+          var indexPoint = i; // indexPoint to build future off of
         } else {
           if (i > indexPoint) {
             timeblockClass[i].classList.add("future");
-          } else {
+          } else { // if indexPoint DNE
             timeblockClass[i].classList.add("past");
           }
         }
@@ -93,5 +87,5 @@ $(function () {
   $('#currentDay').text(today.format('dddd, MMMM D, YYYY'));
 
   colorCode();
-  enterEvent();
+  saveEvent();
 });
